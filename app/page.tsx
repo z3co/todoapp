332e481c4a47a774327280f7264587a2fab5a698
Jeppe Wolff Johansen <Jepper123411@proton.me>
2025-03-16 13:06:08 +0100

    Added a homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
 import Image from "next/image";
-import { Link } from "next/navigation";
-import { lusitana } from "@/app/ui/fonts";
+import Link from "next/link";
+import { lusitana, inter } from "@/app/ui/fonts";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
 	return (
 		<main>
-			<div className={`${lusitana.className} flex flex-col items-center justify-center h-screen `}>
-        <div className="relative bg-blue-500 border-t-[15px] border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-transparent border-t-transparent mb-80 rounded-3xl gap-6" >
-					<p className="text-center text-6xl text-gray-200">TodoApp</p>
-					<p className="text-4xl text-gray-300 mt-4 ">
+			<div
+				className={`${lusitana.className} flex flex-col items-center mt-5 md:mt-30`}
+			>
+				<div className="relative bg-blue-500 border-t-[15px] border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-transparent border-t-transparent rounded-3xl gap-6">
+					<p className="text-center text-4xl md:text-6xl text-gray-200">TodoApp</p>
+					<p className="text-xl md:text-4xl text-gray-300 mt-4 ">
 						The best way to manage your day
 					</p>
-        </div>
+				</div>
+				<div className="flex flex-col items-center">
+					<div className="m-30 md:m-50">
+						{/* Put pictures of the main dashboard here */}
+					</div>
+					<Link
+						href="/dashboard"
+						className={`${inter.className} flex items-center gap-5 m-4 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base`}
+					>
+						<span>Get started</span> <ArrowRightIcon className="w-5 md:w-6" />
+					</Link>
+				</div>
 			</div>
 		</main>
 	);
